c53c185faeb247e7196824580a1d1bd0f17ea1ff
PROTOTYPE8768 <jmfescala@gmail.com>
2024-09-10 00:30:53 +0800

    Resolved merge conflicts

---
+++ dist/js/--olbega--animations_script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const containers = document.querySelectorAll('.olvega_container');
+
+    const animations = [
+        'img-fadein', 'img-fadebot', 'img-fadebotbig', 'img-FadeInLeft',
+        'img-FadeInLeftBig', 'img-FadeInRight', 'img-FadeInRightBig', 
+        'img-FadeInUp', 'img-FadeInUpBig', 'img-Flipx', 'img-Flipx-short', 
+        'img-Flipy', 'img-RotateIn', 'img-RotateInDownLeft', 
+        'img-RotateInDownRight', 'img-RotateInUpLeft', 'img-RotateInUpRight',
+        'img-ZoomIn', 'img-ZoomInDown', 'img-ZoomInLeft', 'img-ZoomInRight', 
+        'img-ZoomInUp', 'img-BounceIn', 'img-BounceInDown', 
+        'img-BounceInLeft', 'img-BounceInRight', 'img-BounceInUp'
+    ];
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const index = Array.from(entry.target.parentNode.children).indexOf(entry.target);
+                const img = entry.target.querySelector('img');
+                img.classList.add(animations[index]);
+            }
+        });
+    }, {
+        threshold: 0.5  
+    });
+
+    containers.forEach(container => {
+        observer.observe(container);
+    });
+});
